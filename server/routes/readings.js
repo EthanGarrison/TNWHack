cb1d34b5540ae27e0bedd1249b35fd823d879952
Julie Gauthier <juliegauthiernyc@gmail.com>
2015-11-15 09:52:34 -0500

    style

--- server/routes/readings.js
+++ server/routes/readings.js
@@ -3,17 +3,30 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var todoist = require('../services/todoist.js');
 
+var limit = 30;
+var sensor = "lux";
+var taskName = "Pay electric bill";
+
 /* GET readings from sensor. */
 router.get('/readings', function(req, res) {
   	res.json(res.body);
+  	limit = 40;
 });
 
 router.post('/readings', function(req, res) {
 	console.log(req.body);
 	res.json(req.body);
-	if (req.body.lux != null && req.body.lux <= 30) {
-		todoist.createItem("Pay electric bill");
+	console.log(limit);
+	if (req.body[sensor] != null && req.body[sensor] <= limit) {
+		todoist.createItem(taskName);
 	}
 });
+
+router.post('/userVar', function(req, res) {
+	limit = req.body.limit;
+	sensor = req.body.sensor;
+	taskName = req.body.taskName;
+	res.end();
+})
 
 module.exports = router;
