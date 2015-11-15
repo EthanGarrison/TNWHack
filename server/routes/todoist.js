67f3285bacbb7f8435d899580c8ead8ed16228ae
Ethan Garrison <egarrison@varickmm.com>
2015-11-14 22:39:33 -0500

    Got the promise object working.  Can now build rest of necessary functions
    Signed-off-by: Ethan Garrison <egarrison@varickmm.com>

--- server/routes/todoist.js
+++ server/routes/todoist.js
@@ -10,7 +10,51 @@ var todoist = require('../services/todoist');
 // });
 
 router.get('/getItems', function(req, res) {
-	res.json({"data": todoist.getItems()})
+	todoist.getItems().then(function(data) {
+		res.send(data);
+		console.log("Resolved the promise");
+	},
+	function(error) {},
+	function(notify) {
+		console.log(notify);
+	}).done();
+	// var postData = querystring.stringify({
+	// 	'token' : 'e3ce717c188dec1e93be254aac9709859438a609',
+	// 	'seq_no' : 0,
+	// 	'resource_types' : '["items"]'
+	// });
+
+	// var options = {
+	// 	hostname: 'todoist.com',
+	// 	path: '/API/v6/sync',
+	// 	method: 'POST',
+	// 	headers: {
+	// 		'Content-Type': 'application/x-www-form-urlencoded',
+	// 		'Content-Length': postData.length
+	// 	}
+	// };
+
+	// var request = https.request(options, function(response) {
+	// 	var output = '';
+	// 	response.setEncoding('utf8');
+	// 	response.on('data', function (chunk) {
+	// 		console.log('BODY: ' + chunk);
+	// 		output += chunk;
+	// 	});
+	// 	response.on('end', function() {
+	// 		console.log('No more data in response.')
+	// 		res.send(output);
+	// 	});
+	// });
+
+	// request.on('error', function(e) {
+	//   console.log('problem with request: ' + e.message);
+	// });
+
+	// // write data to request body
+	// request.write(postData);
+	// request.end();
+	// res.json({"data": "Hello"});
 });
 
 module.exports = router;
--- server/services/todoist.js
+++ server/services/todoist.js
@@ -1,17 +1,19 @@
 var https = require('https');
 var querystring = require('querystring');
+var q = require('q');
 
 var todoist = {};
 
-var options = {
+todoist.getItems = function() {
+	var options = {
 		hostname: 'todoist.com',
 		path: '/API/v6/sync',
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded'		}
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
 	};
 
-todoist.getItems = function() {
 	var postData = querystring.stringify({
 		'token' : 'e3ce717c188dec1e93be254aac9709859438a609',
 		'seq_no' : 0,
@@ -20,20 +22,34 @@ todoist.getItems = function() {
 
 	options.headers['Content-Length'] = postData.length;
 
-	var output = '';
+	var output = q.defer();
 
 	var request = https.request(options, function(response) {
+
+		var result = '';
+
 		response.setEncoding('utf8');
 		response.on('data', function (chunk) {
 			console.log('BODY: ' + chunk);
-			output += chunk;
+			result += chunk;
 		});
 		response.on('end', function() {
 			console.log('No more data in response.')
+			output.resolve(result);
+			console.log(result);
 		});
+		// output.resolve(result);
 	});
 
-	return output;
+	request.write(postData);
+	request.end();
+
+	console.log("Got into getItems");
+	// request.on('error', function() {
+	// 	output.reject("Bad request");
+	// })
+
+	return output.promise;
 }
 
 module.exports = todoist;
